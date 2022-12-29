f45c0cf23a9f6f8540098e720e4fe2a770b87584
Administrator <sody15963@gmail.com>
2022-12-29 21:11:53 +0900

    20221229 분할클래스

---
+++ class01/NPC01.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class01
+{
+    partial class NPC01
+    {
+        public int health = 100;
+
+
+    }
+}
--- class01/Program.cs
+++ class01/Program.cs
@@ -4,68 +4,38 @@ using System.Collections.Generic;
 
 namespace class01
 {
-    class Shop
+
+    internal class NPC
     {
-       virtual public void Purchase()
-        {
-            Console.WriteLine("구매");
-        }
-
-        virtual public void Sale()
-        {
-            Console.WriteLine("판매");
-        }
-    }
-
-    class Equipment : Shop
-    {
-        public override void Purchase()
-        {
-            Console.WriteLine("장비구매");
-        }
-        public override void Sale()
-        {
-            Console.WriteLine("장비판매");
-        }
-
-    }
-
-    class CashShop : Shop
-    {
-        public override void Purchase()
-        {
-            Console.WriteLine("캐쉬구매");
-        }
-
-        public sealed override void Sale()
-        {
-            Console.WriteLine("캐쉬판매");
-        }
-    }
-
-    class EventShop : CashShop
-    {
-        public override void Purchase()
-        {
-            Console.WriteLine("이벤트구매");
-        }
-
-        //public sealed override void Sale() // => 에러뜸
-        //{
-        //    Console.WriteLine("캐쉬판매");
-        //}
-    }
-    internal class Program
-    {
+        //delegate void CallBack(); // 델리게이트 선언
         static void Main(string[] args)
         {
-           // sealed : class 또는 method가 다른 클래스에서 상속되지 않도록 막아주는 키워드.
+            #region 분할클래스 
+            /*
+            // 분할클래스 : 클래스 구현이 길어질 경우 2개 이상의 소스 파일로 분할하여 동시에 작업을 수행하거나 관리의 편의를 위해 분할하는 클래스. 
 
-           EventShop eventshop = new EventShop();
-           eventshop.Purchase();
-           eventshop.Sale();
+            NPC01 npc = new NPC01();
+            npc.health = 100;
+            npc.Dialogue();
+            */
+            #endregion
+
+
+            //CallBack callback = () => Console.WriteLine("안녕"); // 람다식 무명메소드 
+            //callback();
+
+            // action : 반환형 메소드가 없는 메소드를 저장할 수 있는 delegate (고급문법), 매개변수를 최대 16개까지 지정할 수 있다.
+            //Action<int> action = (int a) => Console.WriteLine("Action 입니다");
+            //action(10);
+
+            //// Func : 반환형이 존재하는 메소드를 저장할 수 있는 delegate (고급문법), Func<매개변수, 반환형>
+
+            //Func<int, int> func = (int a) => 10;
+            //Console.WriteLine("Func의 값: " + func(10));
+            
 
         }
 
     }
 }
+
