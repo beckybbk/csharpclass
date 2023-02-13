3a61ab88133fd800ab0ee3cf5e8ffcf0e36ab98b
Administrator <sody15963@gmail.com>
2023-02-13 21:58:14 +0900

    포톤서버

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+
+namespace ConsoleApp1
+{
+    enum WEEK
+    {
+        SUN,
+        MON,
+        TUE, 
+        WED,
+        THU,
+        FRI,
+        SAT,
+    }
+    internal class Program
+    {
+        static void solution(int a, int b)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+}
